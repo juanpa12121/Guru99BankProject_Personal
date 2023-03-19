a47862000aa1863f7cc7847ff1bde416b8d17b8d
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2023-03-19 11:47:07 -0500

    Change Given from StepDefinitions, call driver without having MyDriversWeb class

--- src/test/java/co/com/bancolombia/certification/guru99/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/co/com/bancolombia/certification/guru99/stepdefinitions/LoginStepDefinitions.java
@@ -10,15 +10,23 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
+
+import static co.com.bancolombia.certification.guru99.utils.Constants.URL_PAGE;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.annotations.Managed;
 import org.hamcrest.Matchers;
+import org.openqa.selenium.WebDriver;
 
 import java.util.List;
 
 public class LoginStepDefinitions {
+
+    @Managed
+    WebDriver driver;
 
     @Before
     public void setUp(){
@@ -28,7 +36,8 @@ public class LoginStepDefinitions {
 
     @Given("^I am on the login page$")
     public void iAmOnTheLoginPage() {
-        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(MyDriversWeb.web().inThePageWeb(Constants.URL)));
+        theActorInTheSpotlight().can(BrowseTheWeb.with(driver));
+        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url(URL_PAGE));
     }
 
 
